c078df8b837b647b50cf59ce62810c7131be88d6
badrxd <badrxdxd@gmail.com>
2024-03-03 21:43:05 +0100

    fix component name

--- app/about/page.js
+++ app/about/page.js
@@ -2,7 +2,7 @@
 import React from 'react'
 import { useSession, signIn, signOut } from "next-auth/react"
 
-function page() {
+function Page() {
     const { data: session } = useSession()
 
     if (session) {
