346d864d340ea6ebf97221265c51343c6a4303d8
badrxd <badrxdxd@gmail.com>
2024-03-04 01:19:02 +0100

    delete env

--- app/api-doc/react-swagger.tsx
+++ app/api-doc/react-swagger.tsx
@@ -1,14 +1,9 @@
 "use client";
 
-import 'swagger-ui-react/swagger-ui.css';
+import SwaggerUI from "swagger-ui-react";
+import "swagger-ui-react/swagger-ui.css";
 
-import SwaggerUI from 'swagger-ui-react';
-
-type Props = {
-  spec: Record<string, any>;
-};
-
-function ReactSwagger({ spec }: Props) {
+function ReactSwagger({ spec }) {
   return <SwaggerUI spec={spec} />;
 }
 
