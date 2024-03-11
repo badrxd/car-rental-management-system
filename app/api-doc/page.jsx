f9c28e8adb9c06100540f0dfa8abd93e9b917056
badrxd <badrxdxd@gmail.com>
2024-03-11 13:36:56 +0000

    update

--- app/api-doc/page.jsx
+++ app/api-doc/page.jsx
@@ -4,8 +4,10 @@ import { getApiDocs } from '@/lib/swagger';
 export default async function ApiDocPage() {
   const spec = await getApiDocs();
   return (
-    <section className="w-full">
+    <div>
+      {/* <section className="w-full">
       <ReactSwagger spec={spec} />
-    </section>
+    </section> */}
+    </div>
   );
 }
