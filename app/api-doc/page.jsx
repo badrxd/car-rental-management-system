d0942ab3b69672b852f85cad72d7a4ca91ccda31
badrxd <badrxdxd@gmail.com>
2024-03-11 14:07:43 +0000

    check again swagger

--- app/api-doc/page.jsx
+++ app/api-doc/page.jsx
@@ -1,13 +1,13 @@
-import ReactSwagger from './react-swagger';
-import { getApiDocs } from '@/lib/swagger';
+import ReactSwagger from "./react-swagger";
+import { getApiDocs } from "@/lib/swagger";
 
 export default async function ApiDocPage() {
   const spec = await getApiDocs();
   return (
     <div>
-      {/* <section className="w-full">
-      <ReactSwagger spec={spec} />
-    </section> */}
+      <section className="w-full">
+        <ReactSwagger spec={spec} />
+      </section>
     </div>
   );
 }
