d8bcec87c6f3bb93d18e84294f5974f25ce13885
badrxd <badrxdxd@gmail.com>
2024-03-15 02:51:45 +0000

    middleware

--- middleware.js
+++ middleware.js
@@ -1,3 +1,25 @@
-export { default } from "next-auth/middleware"
+export { default } from "next-auth/middleware";
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
 
-export const config = { matcher: ["/dashboard"] }
+export async function middleware(req) {
+  const { pathname, origin } = req.nextUrl;
+
+  const session = await getToken({
+    req,
+    secret: process.env.JWT_SECRET,
+    secureCookie: process.env.NODE_ENV === "production",
+  });
+  if (pathname.startsWith("/api/privet")) {
+    if (!session) return NextResponse.redirect(`${origin}`);
+  }
+  if (pathname.startsWith("/dashboard")) {
+    if (!session) return NextResponse.redirect(`${origin}/login`);
+  }
+
+  if (pathname.startsWith("/login")) {
+    if (session) return NextResponse.redirect(`${origin}/dashboard`);
+  }
+}
+
+// export const config = { matcher: ["/dashboard"] };
