978049dcc2f1ba068988abd946b7a75d48ecb2a5
badrxd <badrxdxd@gmail.com>
2024-03-15 23:54:40 +0000

    change dachboard api to be dynamic, will not cach any data

--- app/api/privet/dashboard/route.js
+++ app/api/privet/dashboard/route.js
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server";
 import prisma from "@/prisma/prisma";
 import Validator from "@/lib/backEnd/inputValidation";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(request) {
   const dash = {
     mounth: {
