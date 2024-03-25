4bd934aefc39c9db55e6096da021abd6a318c546
Soukacoddy <125686048+Soukacoddy@users.noreply.github.com>
2024-03-25 04:22:20 +0000

    fix the map key in popularcars comp

--- components/Popularcars.jsx
+++ components/Popularcars.jsx
@@ -4,8 +4,8 @@ import Carcard from "@/components/Carcard";
 export default function Popularcars({ data }) {
   return (
     <div className="flex justify-between pt-10 gap-4 flex-col lg:flex-row md:flex-row">
-      {data.map((item) => {
-        return <Carcard item={item} />;
+      {data.map((item ,key) => {
+        return <Carcard item={item} key={key} />;
       })}
     </div>
   );
