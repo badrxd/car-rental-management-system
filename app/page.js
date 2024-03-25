1eb7b5ad72c57453ff0b7c9281d31880eee5e35f
Soukacoddy <125686048+Soukacoddy@users.noreply.github.com>
2024-03-25 04:09:41 +0000

    Add random selection for the best data car

--- app/page.js
+++ app/page.js
@@ -3,20 +3,27 @@ import Popularcars from "@/components/Popularcars";
 import Cars from "@/components/Cars";
 import Howitwork from "@/components/Howitwork";
 import BestCar from "@/components/BestCar";
+import Link from "next/link";
 
 export default async function Home() {
-  const CarData = await fetch(`${process.env.NEXTAUTH_URL}/api/public/cars`);
+  const CarData = await fetch(`${process.env.NEXTAUTH_URL}/api/public/cars`, {
+    cache: "no-store",
+  });
   const result = await CarData.json();
-  console.log(result);
   return (
     <main className="max-w-6xl mx-auto">
       <Hero />
       <h1 className="font-bold text-3xl mt-5">Our Popular Cars</h1>
-      <Popularcars />
+      <Popularcars data={result.top_cars} />
       <Howitwork />
-      <h1 className="font-bold text-3xl mt-5">Resent Added Cars</h1>
-      <Cars />
-      <BestCar />
+      <BestCar data={result.allCars} />
+      <h1 className="font-bold text-3xl mt-5">Available Cars</h1>
+      <Cars data={result.allCars} />
+      <div className="flex justify-center items-center mt-5">
+        <button className="bg-[#000000] hover:bg-[#454545] w-40 text-white rounded-full p-2">
+          <Link href={"/cars"}>Show More</Link>
+        </button>
+      </div>
     </main>
   );
 }
