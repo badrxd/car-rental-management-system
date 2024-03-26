bee2e400b4afd06b13b3739c35e9c8b31d9be4c7
Soukacoddy <125686048+Soukacoddy@users.noreply.github.com>
2024-03-26 05:02:53 +0000

    add api data to the reservation preview page and add styling

--- app/dashboard/reservations/[id]/page.jsx
+++ app/dashboard/reservations/[id]/page.jsx
@@ -1,36 +1,79 @@
+"use client";
 import Image from "next/image";
 import React from "react";
+import useSWR from "swr";
 
-export default async function Page({ params }) {
-  const data = await fetch(
-    `${process.env.NEXTAUTH_URL}/api/privet/reservations/${params.id}`
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+export default function Page({ params }) {
+  // const data = await fetch(
+  //   `${process.env.NEXTAUTH_URL}/api/privet/reservations/${params.id}`
+  // );
+  // const result = await data.json();
+  // console.log(result);
+  const { data, error, isLoading } = useSWR(
+    `${process.env.NEXT_PUBLIC_URL}/api/privet/reservations/${params.id}`,
+    fetcher
   );
-  const result = await data.json();
-  console.log(result);
+  if (isLoading) {
+    return <div>Loading</div>;
+  }
+  if (error) {
+    return <div>error fetching data</div>;
+  }
+
+  const { Car, Customer, Date_range, amount, status } = data.reservation;
+  const date1 = new Date(Date_range.start_date);
+  const date2 = new Date(Date_range.end_date);
+  const differenceInMilliseconds = Math.abs(date2 - date1);
+  const differenceInDays = differenceInMilliseconds / (1000 * 60 * 60 * 24);
+  const rentedddays = Math.round(differenceInDays);
+
   return (
     <div className="flex p-5 mt-5">
-      <div className="w-full p-5 flex flex-col justify-between gap-4">
-        <div className="bg-[#fff] flex justify-between gap-2 p-4 rounded-2xl">
-          <Image src={"/car.png"} width={500} height={500} alt="rev_car" />
-          <div>
-            <h1>car name</h1>
-            <h1>brand</h1>
-            <h1>gear box</h1>
-            <h1>fuels</h1>
-          </div>
-        </div>
+      <div className="w-full p-5 flex flex-col justify-between">
         <div className="bg-[#fff] flex gap-2 p-4 rounded-2xl">
           <Image
-            src={"/avatar.png"}
-            width={80}
-            height={80}
-            alt="avatar"
-            className="rounded-full"
+            src={`${process.env.NEXT_PUBLIC_IMAGE_URL}/${Car?.image}`}
+            width={500}
+            height={303}
+            alt="rev_car"
           />
-          <div>
-            <h1>cutomer name</h1>
-            <h1>driver id</h1>
-            <h1>phone number</h1>
+          <div className="flex flex-col justify-center">
+            <h1 className="text-[30px] font-bold uppercase">{Car.model}</h1>
+            <h1 className="text-xl">
+              Brand: <span className="uppercase">{Car.brand}</span>
+            </h1>
+            <h1 className="text-xl">Matricule: {Car.matricule}</h1>
+            <h1 className="text-xl">Gear Box: {Car.gear_box}</h1>
+            <h1 className="text-xl">Fuels: {Car.fuels}</h1>
+          </div>
+        </div>
+        <div className="bg-[#fff] flex justify-between gap-6 p-4 rounded-2xl">
+          <div className="flex gap-4 items-center">
+            <Image
+              src={"/avatar.png"}
+              width={80}
+              height={80}
+              alt="avatar"
+              className="rounded-full"
+            />
+            <div>
+              <h1 className="text-[20px] font-bold uppercase">
+                {Customer.full_name}
+              </h1>
+              <h1>Driver ID: {Customer.driver_id}</h1>
+              <h1>Phone Number: {Customer.phone}</h1>
+            </div>
+          </div>
+          <div className="bg-[#fff] p-5 rounded-2xl flex flex-col gap-3">
+            <h1 className="font-bold">Total Amount</h1>
+            <h1>{amount} DH</h1>
+            {status === "CONFIRMED" ? (
+              <button className="bg-[#000] text-[#fff] p-2 rounded-full w-40">
+                Cancel
+              </button>
+            ) : null}
           </div>
         </div>
       </div>
@@ -38,31 +81,33 @@ export default async function Page({ params }) {
         <div className="bg-[#fff] flex flex-col justify-center items-center p-4 rounded-2xl">
           <div>
             <h1 className="bg-[#000] text-[#fff] p-2 rounded-full w-40 text-center">
-              12/02/2024
+              {Date_range.start_date.slice(0, 10)}
             </h1>
           </div>
           <div>|</div>
           <div>|</div>
           <div>|</div>
           <div>|</div>
-          <div>6 Days</div>
+          <div>|</div>
+          <div>|</div>
+          <div>|</div>
+          <div>|</div>
+          <div className="font-bold">{rentedddays} Days</div>
+          <div>|</div>
+          <div>|</div>
+          <div>|</div>
+          <div>|</div>
+          <div>|</div>
+          <div>|</div>
           <div>|</div>
           <div>|</div>
           <div>|</div>
           <div>|</div>
           <div>
             <h1 className="bg-[#000] text-[#fff] p-2 rounded-full w-40 text-center">
-              20/02/2024
+              {Date_range.end_date.slice(0, 10)}
             </h1>
           </div>
-        </div>
-        <div className="bg-[#fff] p-5 rounded-2xl flex flex-col gap-3">
-          <h1>Total Amount</h1>
-          <h1>Rented Days</h1>
-          <h1>1400 DH</h1>
-          <button className="bg-[#000] text-[#fff] p-2 rounded-full w-40">
-            Cancel
-          </button>
         </div>
       </div>
     </div>
